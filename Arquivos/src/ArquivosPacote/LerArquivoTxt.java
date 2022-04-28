4dba9bd3a194b1cebdd368890a6b3a570aff0078
Leona <Leona@192.168.1.120>
2022-04-28 19:58:35 -0300

    16.2 - Lendo as linhas do arquivo de TXT

---
+++ Arquivos/src/ArquivosPacote/LerArquivoTxt.java
@@ -0,0 +1,22 @@
+package ArquivosPacote;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class LerArquivoTxt {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		
+	FileInputStream entradaArquivo = 
+			new FileInputStream(new File("C:\\Users\\Leona\\git\\repository1\\Arquivos\\src\\ArquivosPacote\\arquivo.txt"));
+	
+	Scanner lerArquivo = new Scanner(entradaArquivo, "UTF-8");
+	while(lerArquivo.hasNext()) {
+		String linha = lerArquivo.nextLine(); 
+		if(linha != null && !linha.isEmpty()) {
+		System.out.println(linha);
+	}
+	}}
+}
